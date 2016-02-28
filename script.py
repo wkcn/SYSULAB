e2decd9747927658cbc9846a0f79c3a8eaedfd04
zing <zinglee@vip.qq.com>
2016-02-28 14:32:52 +0800

    Change `need` to the path needed(without blank).

--- script.py
+++ script.py
@@ -1,18 +1,28 @@
 # -*- coding:utf-8 -*-
-import os
+import os,urllib2
 
 def downloadBAT():
-    url = ""
+    url='https://raw.githubusercontent.com/wkcn/SYSULAB/master/AddENV.bat'
+    try:
+        content = urllib2.urlopen(url).read()
+        f = open("AddENV.bat", "w")
+        f.write(content)
+        f.close()
+        return True
+    except Exception, e:
+        return False
 
 
 def main():
-    downloadBAT()
-    need = r"E:\nasm"
-    cmd = '%s\\AddENV.bat %s' % (os.getcwd(), need)
-    print cmd
-    # ret = os.popen(cmd).readlines()
-    # print ret
-    return 0
+    if downloadBAT():
+        need = r"E:\nasm" # 要添加的环境变量，没有空格
+        cmd = '%s\\AddENV.bat %s' % (os.getcwd(), need)
+        # print cmd
+        ret = os.popen(cmd).readlines()
+        # print ret
+        return 0
+    else:
+        return 1
 
 if __name__ == '__main__':
     main()
